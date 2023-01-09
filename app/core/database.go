d918cf25936e3e11204c6bd0f53bad6252255e8d
Tim <sarrtim@gmail.com>
2023-01-09 10:28:27 +0300

    Update print

--- app/core/database.go
+++ app/core/database.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -14,7 +15,7 @@ func ConnectToDb() {
 	var err error
 	dsn := os.Getenv("DB_URL")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	print(DB)
+	fmt.Println(DB)
 
 	if err != nil {
 		log.Fatal("Failed to connect to the datbase")

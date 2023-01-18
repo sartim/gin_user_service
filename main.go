dafe30ce8e5a8038072593e3a90e015f1e63fb08
Tim <sarrtim@gmail.com>
2023-01-18 15:02:20 +0300

    Add custom logging

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	core "gin-shop-api/app/core"
 	"gin-shop-api/app/routes"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +15,23 @@ func init() {
 }
 
 func main() {
+	gin.ForceConsoleColor()
 	r := gin.Default()
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	r.Use(gin.Recovery())
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"status": "ok",

5936d09950973361ac8dc8604790d46e9f4df04a
Tim <sarrtim@gmail.com>
2023-01-18 14:57:36 +0300

    Update email field

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -28,11 +28,11 @@ func UserGetByID(c *gin.Context) {
 }
 
 type UserSchema struct {
-	FirstName string `json:"first_name" binding:"required"`
-	LastName  string `json:"last_name" binding:"required"`
-	Email     string `json:"email" binding:"required"`
-	Password  string `json:"password" binding:"required"`
-	IsActive  bool   `json:"is_active" binding:"required"`
+	FirstName string  `json:"first_name" binding:"required"`
+	LastName  string  `json:"last_name" binding:"required"`
+	Email     *string `json:"email" binding:"required"`
+	Password  string  `json:"password" binding:"required"`
+	IsActive  bool    `json:"is_active" binding:"required"`
 }
 
 func UserCreate(c *gin.Context) {

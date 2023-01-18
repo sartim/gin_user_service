705f62dfc11125cd7b5a8a898b13359ee7255d51
Tim <sarrtim@gmail.com>
2023-01-18 14:56:28 +0300

    Add unique index to status name field

--- app/models/status.go
+++ app/models/status.go
@@ -1,11 +1,15 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"database/sql"
+
+	"gorm.io/gorm"
+)
 
 type Status struct {
 	gorm.Model
-	ID          uint   `json:"id" gorm:"primary_key"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Deleted     bool   `json:"deleted"`
+	ID          uint           `json:"id" gorm:"primary_key"`
+	Name        string         `json:"name" gorm:"index:idx_name,unique"`
+	Description sql.NullString `json:"description"`
+	Deleted     *bool          `json:"deleted" gorm:"defult=false"`
 }

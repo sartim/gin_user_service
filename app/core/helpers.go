6379435daf8f03599cf7674a39edb9814d1afb4d
Tim <sarrtim@gmail.com>
2023-01-23 12:08:39 +0300

    Add logging helpers

--- app/core/helpers.go
+++ app/core/helpers.go
@@ -7,20 +7,28 @@ import (
 
 func LogEvent(logLevel string) *log.Logger {
 	// Info writes logs in the color blue with "INFO: " as prefix
-	// var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
+	var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
 
 	// Warning writes logs in the color yellow with "WARNING: " as prefix
-	// var Warning = log.New(os.Stdout, "\u001b[33mWARNING: \u001B[0m", log.LstdFlags|log.Lshortfile)
+	var Warning = log.New(os.Stdout, "\u001b[33mWARNING: \u001B[0m", log.LstdFlags|log.Lshortfile)
 
 	// Error writes logs in the color red with "ERROR: " as prefix
 	var Error = log.New(os.Stdout, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
 
 	// Debug writes logs in the color cyan with "DEBUG: " as prefix
-	// var Debug = log.New(os.Stdout, "\u001b[36mDEBUG: \u001B[0m", log.LstdFlags|log.Lshortfile)
+	var Debug = log.New(os.Stdout, "\u001b[36mDEBUG: \u001B[0m", log.LstdFlags|log.Lshortfile)
 
-	if logLevel == "ERROR" {
+	switch logLevel {
+	case "INFO":
+		return Info
+	case "WARNING":
+		return Warning
+	case "ERROR":
 		return Error
+	case "DEBUG":
+		return Debug
 	}
+
 	return nil
 }
 

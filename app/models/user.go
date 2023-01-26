4e777bc1a7dfad02c13d3cebbfded07a9ab701bf
Tim <sarrtim@gmail.com>
2023-01-26 15:31:38 +0300

    Update email field on user model

--- app/models/user.go
+++ app/models/user.go
@@ -10,7 +10,7 @@ type User struct {
 	ID        uuid.UUID `json:"id" gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
-	Email     *string   `json:"email" gorm:"index:idx_name,unique"`
+	Email     string    `json:"email" gorm:"index:idx_name,unique"`
 	Password  string    `json:"password"`
 	IsActive  bool      `json:"is_active" gorm:"defult=false"`
 	Deleted   bool      `json:"deleted" gorm:"defult=false"`

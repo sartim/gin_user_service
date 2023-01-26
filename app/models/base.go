36260a7ef6e4f1f0159c628225d27e9306b455c8
Tim <sarrtim@gmail.com>
2023-01-26 18:10:17 +0300

    Add base model struct

---
+++ app/models/base.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Model struct {
+	CreatedAt time.Time      `json:"created_at" gorm:"column:password"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"column:updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"column:deleted_at;index"`
+}

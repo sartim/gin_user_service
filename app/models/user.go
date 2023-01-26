657eea56c68ad9bb60181741cbdb75401782a64e
Tim <sarrtim@gmail.com>
2023-01-26 18:10:30 +0300

    Update user model struct

--- app/models/user.go
+++ app/models/user.go
@@ -2,16 +2,15 @@ package models
 
 import (
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
-	ID        uuid.UUID `json:"id" gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email" gorm:"index:idx_name,unique"`
-	Password  string    `json:"password"`
-	IsActive  bool      `json:"is_active" gorm:"defult=false"`
-	Deleted   bool      `json:"deleted" gorm:"defult=false"`
+	Model
+	ID        uuid.UUID `json:"id" gorm:"column:id;primary_key;type:uuid;default:uuid_generate_v4()"`
+	FirstName string    `json:"first_name" gorm:"column:first_name"`
+	LastName  string    `json:"last_name" gorm:"column:last_name"`
+	Email     string    `json:"email" gorm:"column:email;index:idx_name,unique"`
+	Password  string    `json:"-" gorm:"column:password"`
+	IsActive  bool      `json:"is_active" gorm:"column:is_active;defult=false"`
+	Deleted   bool      `json:"deleted" gorm:"column:deleted;defult=false"`
 }

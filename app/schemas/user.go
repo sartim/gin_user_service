55699876277d39cd85ce3aab30320ff3451c4aef
Tim <sarrtim@gmail.com>
2023-01-28 09:13:33 +0300

    Add user schema to schmas

---
+++ app/schemas/user.go
@@ -0,0 +1,9 @@
+package schemas
+
+type UserSchema struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Email     string `json:"email" binding:"required"`
+	Password  string `json:"password" binding:"required"`
+	IsActive  bool   `json:"is_active" binding:"required"`
+}

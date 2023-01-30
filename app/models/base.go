03aa4c5c62211813c8091600c704af8127ff2c25
Tim <sarrtim@gmail.com>
2023-01-30 12:45:01 +0300

    Fix create_At field

--- app/models/base.go
+++ app/models/base.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Model struct {
-	CreatedAt time.Time      `json:"created_at" gorm:"column:password"`
+	CreatedAt time.Time      `json:"created_at" gorm:"column:create_at"`
 	UpdatedAt time.Time      `json:"updated_at" gorm:"column:updated_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"column:deleted_at;index"`
 }

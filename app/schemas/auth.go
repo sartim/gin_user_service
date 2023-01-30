5b44a0e1d5f35ee9828759961defeaa2a957304f
Tim <sarrtim@gmail.com>
2023-01-30 10:21:00 +0300

    Add HeaderSchema

--- app/schemas/auth.go
+++ app/schemas/auth.go
@@ -4,3 +4,7 @@ type AuthSchema struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+
+type HeaderSchema struct {
+	Authorization *string `header:"Authorization" binding:"required"`
+}

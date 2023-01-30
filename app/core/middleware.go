03933a9e3e856543e7c4a1c8e58b46fa950d1470
Tim <sarrtim@gmail.com>
2023-01-30 12:44:48 +0300

    Add require auth middleware

--- app/core/middleware.go
+++ app/core/middleware.go
@@ -1,7 +1,16 @@
 package core
 
 import (
+	"fmt"
+	"gin-shop-api/app/models"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
@@ -18,4 +27,54 @@ func CORSMiddleware() gin.HandlerFunc {
 
 		ctx.Next()
 	}
+}
+
+func RequireAuth(c *gin.Context) {
+	authorizationValue := c.Request.Header.Get("Authorization")
+	authorizationValues := strings.Fields(authorizationValue)
+	tokenString := authorizationValues[1]
+
+	// Parse takes the token string and a function for looking up the key.
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// check the exp
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		// find the user with the token sub
+		var user = models.User{ID: uuid.Must(uuid.Parse(fmt.Sprint(claims["sub"])))}
+		fmt.Println(user)
+
+		DB.First(&user)
+
+		if user.ID == uuid.Nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		// Attach to req
+		c.Set("user", user)
+
+		// Continue
+		c.Next()
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	c.Next()
+
 }

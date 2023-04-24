a8800f4ad6815a35999273d10f3fe8bdebdaf85e
Tim <sarrtim@gmail.com>
2023-04-24 12:28:30 +0300

    Add permission model

--- internal/models/permission.go
+++ internal/models/permission.go
@@ -1,15 +1,13 @@
 package models
 
 import (
-	"database/sql"
-
 	"gorm.io/gorm"
 )
 
 type Permission struct {
 	gorm.Model
-	ID          uint           `json:"id" gorm:"primary_key"`
-	Name        string         `json:"name" gorm:"index:idx_name,unique"`
-	Description sql.NullString `json:"description"`
-	Deleted     *bool          `json:"deleted" gorm:"default=false"`
+	ID          uint   `json:"id" gorm:"primary_key"`
+	Name        string `json:"name" gorm:"index:idx_name,unique"`
+	Description string `json:"description"`
+	Deleted     bool   `json:"deleted" gorm:"default=false"`
 }

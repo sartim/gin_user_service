9b27a8462386b364cd78e5831ec81a9dc658c672
Tim <sarrtim@gmail.com>
2023-04-24 12:15:03 +0300

    Add role model

--- internal/models/role.go
+++ internal/models/role.go
@@ -6,10 +6,10 @@ import (
 	"gorm.io/gorm"
 )
 
-type Status struct {
+type Role struct {
 	gorm.Model
 	ID          uint           `json:"id" gorm:"primary_key"`
 	Name        string         `json:"name" gorm:"index:idx_name,unique"`
 	Description sql.NullString `json:"description"`
-	Deleted     *bool          `json:"deleted" gorm:"defult=false"`
+	Deleted     *bool          `json:"deleted" gorm:"default=false"`
 }

e4c031b286498cd1e79caa7d20fc5e563c13e967
Tim <sarrtim@gmail.com>
2023-04-24 12:30:06 +0300

    Add permission controller with all implemented methods

---
+++ internal/controllers/permission.go
@@ -0,0 +1,58 @@
+package controllers
+
+import (
+	"gin-shop-api/internal/helpers"
+	"gin-shop-api/internal/models"
+	"gin-shop-api/internal/schemas"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type PermissionController struct {
+	*BaseController
+}
+
+func NewPermissionController(db *gorm.DB) *PermissionController {
+	var permission models.Permission
+	var schema schemas.PermissionSchema
+	return &PermissionController{NewBaseController(db, permission, schema)}
+}
+
+func (ctrl *PermissionController) Create(c *gin.Context) {
+	var input schemas.PermissionSchema
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		log.Printf("%s: %s", "Field validation failed", err)
+		helpers.ValidateSchema(c, err, "body")
+		return
+	}
+
+	// Set the hashed password in the permission model
+	permission := models.Permission{
+		Name:        input.Name,
+		Description: input.Description,
+	}
+
+	// Save the permission to the database
+	result := ctrl.db.Create(&permission)
+
+	if result.Error != nil {
+		c.AbortWithStatusJSON(500, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{"permission": permission})
+}
+
+func (ctrl *PermissionController) RegisterPermissionRoutes(router *gin.RouterGroup) {
+	permissionRouter := router.Group("/permissions")
+	{
+		permissionRouter.GET("", ctrl.GetAll)
+		permissionRouter.GET(":id", ctrl.Get)
+		permissionRouter.POST("", ctrl.Create)
+		permissionRouter.PUT(":id", ctrl.Update)
+		permissionRouter.DELETE(":id", ctrl.Delete)
+	}
+}

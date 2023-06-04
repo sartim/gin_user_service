18ca82b71099bdc6b2d6f2b0ddc523a95cf26037
Tim <sarrtim@gmail.com>
2023-06-04 21:29:59 +0300

    Update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,31 @@
 package config
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
+
+const Environment = "ENV"
+const Port = "PORT"
+const DbUrl = "DB_URL"
 
 func DatabaseConfig() string {
 	return os.Getenv("DB_URL")
+}
+
+type Config struct {
+	EnvVar string
+}
+
+func (config Config) Get() string {
+	envVar := os.Getenv(config.EnvVar)
+	return envVar
+}
+
+func CurrDir() string {
+	mydir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return mydir
 }

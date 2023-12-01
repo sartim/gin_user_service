f5511f576a12c405e390ce544b63d0141fb816ce
Tim <sarrtim@gmail.com>
2023-12-01 16:01:01 +0300

    Remove comment

--- tests/user_test.go
+++ tests/user_test.go
@@ -14,7 +14,6 @@ import (
 
 func TestGetUsers(t *testing.T) {
 	Setup()
-	// mockResponse := `{"data":[{"created_at":"0001-01-01T00:00:00Z","updated_at":"2023-01-26T15:11:42.478209+03:00","deleted_at":null,"id":"ab2fc765-cab7-4434-bef2-121bef275572","first_name":"Jane","last_name":"Doe","email":"janedoe@mail.com","is_active":true,"deleted":false}]}`
 	r := SetUpRouter()
 	ctrl := controllers.NewUserController(repository.DB)
 	r.GET("/api/v1/users", ctrl.GetAll)
